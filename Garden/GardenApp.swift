e2b40f42c718fcd02822a0d34074be15871c4d85
samil <studi.msa@gmail.com>
2023-07-17 11:16:20 +0300

    first commit

---
+++ Garden/GardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenApp.swift
+//  Garden
+//
+//  Created by muhammed samil demir on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
